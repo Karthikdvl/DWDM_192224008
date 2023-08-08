5a03cbf6f26b0e9d2238cc072895364143b91800
Karthikdvl <119553175+Karthikdvl@users.noreply.github.com>
2023-08-08 13:51:41 +0530

    Add files via upload

---
+++ histogram008.r
@@ -0,0 +1,12 @@
+#histogram
+#create data for the graph
+v<-c(9,13,21,8,36,22,12,41,31,33,19)
+
+#given the chart flie a name
+png(file="histogram.png")
+
+#create the histogram
+hist(v,xlab = "weight",col="yellow",border = "blue")
+
+#save the file
+dev.off()
